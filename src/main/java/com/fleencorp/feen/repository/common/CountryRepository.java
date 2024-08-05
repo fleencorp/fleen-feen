e69288f680fc1967f663607a310a48c2c842b482
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-08-05 15:28:04 +0100

    1. Added method for finding and retrieving a list of countries. 2. Added response class for preparing data required for creating a calendar. 3. Renamed mappers method. 4. Added repository method for retrieving a list of countries with Page.

--- src/main/java/com/fleencorp/feen/repository/common/CountryRepository.java
+++ src/main/java/com/fleencorp/feen/repository/common/CountryRepository.java
@@ -1,7 +1,11 @@
 package com.fleencorp.feen.repository.common;
 
 import com.fleencorp.feen.model.domain.other.Country;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+  Page<Country> findMany(Pageable pageable);
 }
--- src/main/java/com/fleencorp/feen/service/common/CountryService.java
+++ src/main/java/com/fleencorp/feen/service/common/CountryService.java
@@ -1,9 +1,13 @@
 package com.fleencorp.feen.service.common;
 
+import com.fleencorp.base.model.view.search.SearchResultView;
 import com.fleencorp.feen.model.domain.other.Country;
+import com.fleencorp.feen.model.request.search.CountrySearchRequest;
 import com.fleencorp.feen.model.response.other.CountAllResponse;
 
 public interface CountryService {
+
+  SearchResultView findCountries(CountrySearchRequest searchRequest);
 
   Country getCountry(Long id);
 
