0d950ce40675e71bf27801774e540c6448ce257d
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-08-05 17:28:04 +0100

    Fixed failing app on startup due to missing @Query for findMany() of CountryRepository.

--- src/main/java/com/fleencorp/feen/repository/common/CountryRepository.java
+++ src/main/java/com/fleencorp/feen/repository/common/CountryRepository.java
@@ -4,8 +4,10 @@ import com.fleencorp.feen.model.domain.other.Country;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
 
+  @Query("SELECT c FROM Country c WHERE c.countryId IS NOT NULL ORDER BY c.updatedOn DESC")
   Page<Country> findMany(Pageable pageable);
 }
