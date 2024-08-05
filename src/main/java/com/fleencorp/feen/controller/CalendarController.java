c6064245f9dc7e9c69b17220442961224c8b77b1
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-08-05 12:40:59 +0100

    Created CalendarController for retrieving and creating calendars

--- src/main/java/com/fleencorp/feen/controller/CalendarController.java
+++ src/main/java/com/fleencorp/feen/controller/CalendarController.java
@@ -1,9 +1,20 @@
 package com.fleencorp.feen.controller;
 
+import com.fleencorp.base.model.view.search.SearchResultView;
+import com.fleencorp.base.resolver.SearchParam;
+import com.fleencorp.feen.model.dto.calendar.CreateCalendarDto;
+import com.fleencorp.feen.model.dto.calendar.ShareCalendarWithUserDto;
+import com.fleencorp.feen.model.dto.calendar.UpdateCalendarDto;
+import com.fleencorp.feen.model.request.search.calendar.CalendarSearchRequest;
+import com.fleencorp.feen.model.response.calendar.CreateCalendarResponse;
+import com.fleencorp.feen.model.response.calendar.RetrieveCalendarResponse;
+import com.fleencorp.feen.model.response.calendar.ShareCalendarWithUserResponse;
+import com.fleencorp.feen.model.response.calendar.UpdateCalendarResponse;
+import com.fleencorp.feen.model.response.other.DeleteResponse;
 import com.fleencorp.feen.service.calendar.CalendarService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -14,5 +25,39 @@ public class CalendarController {
 
   public CalendarController(final CalendarService calendarService) {
     this.calendarService = calendarService;
+  }
+
+  @GetMapping(value = "/entries")
+  public SearchResultView findCalendars(@SearchParam CalendarSearchRequest searchRequest) {
+    return calendarService.findCalendars(searchRequest);
+  }
+
+  @GetMapping(value = "/detail/{id}")
+  public RetrieveCalendarResponse findCalendar(@PathVariable(name = "id") Long calendarId) {
+    return calendarService.findCalendar(calendarId);
+  }
+
+  @PostMapping(value = "/create")
+  public CreateCalendarResponse createCalendar(@Valid @RequestBody CreateCalendarDto createCalendarDto) {
+    return calendarService.createCalendar(createCalendarDto);
+  }
+
+  @PutMapping(value = "/update/{id}")
+  public UpdateCalendarResponse updateCalendar(
+      @PathVariable(name = "id") Long calendarId,
+      @Valid @RequestBody UpdateCalendarDto updateCalendarDto) {
+    return calendarService.updateCalendar(calendarId, updateCalendarDto);
+  }
+
+  @DeleteMapping(value = "/delete/{id}")
+  public DeleteResponse deleteCalendar(@PathVariable(name = "id") Long calendarId) {
+    return calendarService.deleteCalendar(calendarId);
+  }
+
+  @PutMapping(value = "/share-with-user/{id}")
+  public ShareCalendarWithUserResponse shareCalendarWithUser(
+      @PathVariable(name = "id") Long calendarId,
+      @Valid @RequestBody ShareCalendarWithUserDto shareCalendarWithUserDto) {
+    return calendarService.shareCalendarWithUser(calendarId, shareCalendarWithUserDto);
   }
 }
