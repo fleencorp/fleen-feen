175e4cfa63ad1591843f60f3e0e3d9fa2367293d
John Adeniran <jasolajohn@gmail.com>
2024-09-17 20:09:21 +0100

    spacing and rewriting objects

--- src/test/java/com/fleencorp/feen/model/domain/other/CountryTest.java
+++ src/test/java/com/fleencorp/feen/model/domain/other/CountryTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class CountryTest {
-
+class CountryTest{
 
     @Test
-    void ensure_country_is_null () {
+    void ensure_country_is_null(){
         //GIVEN
         Country country = null;
 
@@ -19,9 +18,7 @@ class CountryTest {
     @Test
     void ensure_country_title_is_not_null(){
         //GIVEN
-        Country country = Country.builder()
-          .title("Nigeria")
-          .build();
+        Country country = new Country(1L, "Nigeria","NG");
 
         //ASSERT
         assertNotNull(country);
@@ -31,193 +28,101 @@ class CountryTest {
     @Test
     void ensure_country_country_id_not_null(){
         //GIVEN
-        Country country = Country.builder()
-          .countryId(1L)
-          .build();
+        Country country = new Country(1L, "Nigeria","NG");
 
         //ASSERT
         assertNotNull(country);
         assertNotNull(country.getCountryId());
-
     }
 
 
     @Test
-    void ensure_country_objects_are_not_equal() {
-
+    void ensure_country_objects_are_not_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country country2 = Country.builder()
-          .countryId(2L)
-          .title("Canada")
-          .code("CA")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country country2 = new Country(2L, "Canada","CA");
 
         //ASSERT
         assertNotEquals(country1, country2);
     }
 
     @Test
-    void ensure_country_titles_are_not_equal() {
-
+    void ensure_country_titles_are_not_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country country2 = Country.builder()
-          .countryId(2L)
-          .title("Canada")
-          .code("CA")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country country2 = new Country(2L, "Canada","CA");
 
         //ASSERT
         assertNotEquals(country1.getTitle(), country2.getTitle());
     }
 
     @Test
-    void ensure_country_codes_are_not_equal() {
-
+    void ensure_country_codes_are_not_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country country2 = Country.builder()
-          .countryId(2L)
-          .title("Canada")
-          .code("CA")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country country2 = new Country(2L, "Canada","CA");
 
         //ASSERT
         assertNotEquals(country1.getCode(), country2.getCode());
     }
 
     @Test
-    void ensure_country_country_id_are_not_equal() {
-
+    void ensure_country_country_id_are_not_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country country2 = Country.builder()
-          .countryId(2L)
-          .title("Canada")
-          .code("CA")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country country2 = new Country(2L, "Canada","CA");
 
         //ASSERT
         assertNotEquals(country1.getCountryId(), country2.getCountryId());
     }
 
     @Test
-    void ensure_country_objects_are_equal() {
+    void ensure_country_objects_are_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country sameCountry = Country.builder()
-                .countryId(1L)
-                .title("Nigeria")
-                .code("NG")
-                .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country sameCountry = new Country(1L, "Nigeria","NG");
 
         //ASSERT
         assertEquals(country1.getTitle(), sameCountry.getTitle());
         assertEquals(country1.getCountryId(), sameCountry.getCountryId());
         assertEquals(country1.getCode(), sameCountry.getCode());
-
-
     }
 
     @Test
-    void ensure_country_codes_are_equal() {
+    void ensure_country_codes_are_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country sameCountry = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country sameCountry = new Country(1L, "Nigeria","NG");
 
         //ASSERT
         assertEquals(country1.getCode(), sameCountry.getCode());
-
-
     }
 
     @Test
-    void ensure_country_country_ids_are_equal() {
+    void ensure_country_country_ids_are_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country sameCountry = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country sameCountry = new Country(1L, "Nigeria","NG");
 
         //ASSERT
         assertEquals(country1.getCountryId(), sameCountry.getCountryId());
     }
 
     @Test
-    void ensure_country_titles_are_equal() {
+    void ensure_country_titles_are_equal(){
         //GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country sameCountry = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country sameCountry = new Country(1L, "Nigeria","NG");
 
         //ASSERT
         assertEquals(country1.getTitle(), sameCountry.getTitle());
     }
 
     @Test
-    void ensure_country_is_not_null() {
+    void ensure_country_is_not_null(){
         // GIVEN
-        Country country1 = Country.builder()
-          .countryId(1L)
-          .title("Nigeria")
-          .code("NG")
-          .build();
-
-        Country country2 = Country.builder()
-          .countryId(2L)
-          .title("Canada")
-          .code("CA")
-          .build();
+        Country country1 = new Country(1L, "Nigeria","NG");
+        Country country2 = new Country(2L, "Canada","CA");
 
         //ASSERT
         assertNotEquals(country1, null);
