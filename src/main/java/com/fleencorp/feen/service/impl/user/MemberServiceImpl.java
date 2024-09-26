e9b146541eef228ffe8269778e6a7db8e92f5ed5
Yusuf Alamu Musa <alamu@fleencorp.com>
2024-09-26 13:43:04 +0100

    1. Added EmailAddressExistsResponse and EmailAddressNotExistsResponse. 2. Added PhoneNumberExistsResponse and PhoneNumberNotExistsResponse. 3. Added localization messages to messages_en_US.properties and messages_fr.properties. 4. Refactored MemberServiceImpl to return EntityExistsResponse for isMemberEmailAddressExists and isMemberPhoneNumberExists methods. 5. Refactored MiscController to replace use of EmailService and PhoneService with MemberService.

--- src/main/java/com/fleencorp/feen/service/impl/user/MemberServiceImpl.java
+++ src/main/java/com/fleencorp/feen/service/impl/user/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package com.fleencorp.feen.service.impl.user;
 
 import com.fleencorp.base.service.EmailService;
 import com.fleencorp.base.service.PhoneService;
+import com.fleencorp.feen.model.response.common.EmailAddressExistsResponse;
+import com.fleencorp.feen.model.response.common.EmailAddressNotExistsResponse;
+import com.fleencorp.feen.model.response.common.PhoneNumberExistsResponse;
+import com.fleencorp.feen.model.response.common.PhoneNumberNotExistsResponse;
+import com.fleencorp.feen.model.response.other.EntityExistsResponse;
 import com.fleencorp.feen.repository.user.MemberRepository;
+import com.fleencorp.feen.service.i18n.LocalizedResponse;
 import com.fleencorp.feen.service.user.MemberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,14 +25,38 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService, EmailService, PhoneService {
 
   private final MemberRepository memberRepository;
+  private final LocalizedResponse localizedResponse;
 
   /**
    * Constructs a new instance of {@code MemberServiceImpl} with the specified member repository.
    *
    * @param memberRepository the repository used to manage member entities.
    */
-  public MemberServiceImpl(final MemberRepository memberRepository) {
+  public MemberServiceImpl(
+      final MemberRepository memberRepository,
+      final LocalizedResponse localizedResponse) {
     this.memberRepository = memberRepository;
+    this.localizedResponse = localizedResponse;
+  }
+
+  /**
+   * Checks if a member's email address exists in the system.
+   *
+   * <p>This method verifies the existence of the given email address
+   * by calling {@link #isEmailAddressExist(String)}. Depending on whether
+   * the email address exists or not, it returns a localized response
+   * indicating the result.
+   *
+   * @param emailAddress the email address to check for existence
+   * @return an {@link EntityExistsResponse} containing a localized response
+   *         indicating whether the email address exists or not
+   */
+  @Override
+  public EntityExistsResponse isMemberEmailAddressExists(String emailAddress) {
+    boolean exists = isEmailAddressExist(emailAddress);
+    return exists
+      ? localizedResponse.of(EmailAddressExistsResponse.of(true))
+      : localizedResponse.of(EmailAddressNotExistsResponse.of(false));
   }
 
   /**
@@ -38,6 +68,26 @@ public class MemberServiceImpl implements MemberService, EmailService, PhoneServ
   @Override
   public boolean isEmailAddressExist(final String emailAddress) {
     return memberRepository.existsByEmailAddress(emailAddress);
+  }
+
+  /**
+   * Checks if a member's phone number exists in the system.
+   *
+   * <p>This method verifies the existence of the given phone number
+   * by calling {@link #isPhoneNumberExist(String)}. Based on the existence
+   * of the phone number, it returns a localized response indicating
+   * the result.
+   *
+   * @param phoneNumber the phone number to check for existence
+   * @return an {@link EntityExistsResponse} containing a localized response
+   *         indicating whether the phone number exists or not
+   */
+  @Override
+  public EntityExistsResponse isMemberPhoneNumberExists(String phoneNumber) {
+    boolean exists = isPhoneNumberExist(phoneNumber);
+    return exists
+      ? localizedResponse.of(PhoneNumberExistsResponse.of(true))
+      : localizedResponse.of(PhoneNumberNotExistsResponse.of(false));
   }
 
   /**
