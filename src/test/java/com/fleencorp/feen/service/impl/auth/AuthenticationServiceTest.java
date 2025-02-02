90cfe53cc928b02af8556641c452d3b44b3da133
Yusuf Alamu Musa <alamu@fleencorp.com>
2025-02-02 16:18:57 +0100

    1. Refactored MessageSourceConfiguration and SignInResponse. 2. Added unit test for AuthenticationServiceTest

---
+++ src/test/java/com/fleencorp/feen/service/impl/auth/AuthenticationServiceTest.java
@@ -0,0 +1,111 @@
+package com.fleencorp.feen.service.impl.auth;
+
+import com.fleencorp.feen.event.publisher.ProfileRequestPublisher;
+import com.fleencorp.feen.exception.auth.InvalidAuthenticationException;
+import com.fleencorp.feen.mapper.CommonMapper;
+import com.fleencorp.feen.model.dto.auth.SignInDto;
+import com.fleencorp.feen.model.security.FleenUser;
+import com.fleencorp.feen.repository.user.MemberRepository;
+import com.fleencorp.feen.service.common.CountryService;
+import com.fleencorp.feen.service.impl.cache.CacheService;
+import com.fleencorp.feen.service.security.TokenService;
+import com.fleencorp.feen.service.user.MemberService;
+import com.fleencorp.feen.service.user.RoleService;
+import com.fleencorp.localizer.service.Localizer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationServiceTest {
+
+  @Mock
+  AuthenticationManager authenticationManager;
+
+  @Mock
+  MemberService memberService;
+
+  @Mock
+  CacheService cacheService;
+
+  @Mock
+  CountryService countryService;
+
+  @Mock
+  RoleService roleService;
+
+  @Mock
+  TokenService tokenService;
+
+  @Mock
+  MemberRepository memberRepository;
+
+  @Mock
+  PasswordEncoder passwordEncoder;
+
+  @Mock
+  ProfileRequestPublisher profileRequestPublisher;
+
+  @Mock
+  Localizer localizer;
+
+  @Mock
+  CommonMapper commonMapper;
+
+  @InjectMocks
+  AuthenticationServiceImpl authenticationService;
+
+  @Test
+  void ensure_credentials_invalid() {
+    // given
+    String emailAddress = "connor@example.com";
+    String password = "#2#2C--r#2#2";
+
+    SignInDto signInDto = new SignInDto();
+    signInDto.setEmailAddress(emailAddress);
+    signInDto.setPassword(password);
+
+    // when
+    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+      .thenReturn(null);
+
+    // then
+    assertThrows(InvalidAuthenticationException.class, () -> authenticationService.signIn(signInDto));
+  }
+
+  @Test
+  void ensure_credentials_valid() {
+    // given
+    String emailAddress = "connor@example.com";
+    String password = "#2#2C--r#2#2";
+
+    SignInDto signInDto = new SignInDto();
+    signInDto.setEmailAddress(emailAddress);
+    signInDto.setPassword(password);
+
+    FleenUser authorizedUser = new FleenUser();
+    authorizedUser.setEmailAddress(emailAddress);
+
+    Authentication authenticatedUser = new UsernamePasswordAuthenticationToken(authorizedUser, null, List.of());
+
+    // when
+    when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+    .thenReturn(authenticatedUser);
+
+    // then
+    assertDoesNotThrow(() -> authenticationService.signIn(signInDto));
+  }
+}
