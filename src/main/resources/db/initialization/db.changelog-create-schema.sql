25d10ba62c7e51e51cabea040f7fce8a682470c1
Yusuf Alamu Musa <alamu@fleencorp.com>
2025-09-01 05:35:21 -0400

    1. Resolved other possible bugs in dto. 2. Updated column names including music_link and stream_type in db.changelog-create-schema.sql for stream table.

--- src/main/resources/db/initialization/db.changelog-create-schema.sql
+++ src/main/resources/db/initialization/db.changelog-create-schema.sql
@@ -257,11 +257,13 @@ CREATE TABLE stream (
 
   other_details VARCHAR(3000) NULL,
   other_link VARCHAR(1000) NULL,
-  spotify_link VARCHAR(1000) NULL,
+  music_link VARCHAR(1000) NULL,
   group_or_organization_name VARCHAR(500) NULL,
 
   stream_source VARCHAR(255) DEFAULT 'NONE'
     NOT NULL CHECK (stream_source IN ('GOOGLE_MEET', 'GOOGLE_MEET_LIVESTREAM', 'NONE', 'YOUTUBE_LIVE', 'EMAIL', 'PHONE', 'NONE')),
+  stream_type VARCHAR(255)
+    NOT NULL CHECK (stream_creation_type IN ('EVENT', 'LIVE_STREAM')),
   stream_creation_type VARCHAR(255) DEFAULT 'INSTANT'
     NOT NULL CHECK (stream_creation_type IN ('INSTANT', 'SCHEDULED')),
   stream_visibility VARCHAR(255) DEFAULT 'PUBLIC'
